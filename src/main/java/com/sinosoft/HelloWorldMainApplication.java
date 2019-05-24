da97c31bca7fba5fd954e169b642c4bf353591b2
Administrator <cuiyuling110@gmaiol.com>
2019-05-24 14:10:30 +0800

    自己练习一下git

---
+++ src/main/java/com/sinosoft/HelloWorldMainApplication.java
@@ -0,0 +1,15 @@
+package com.sinosoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @SpringBootApplication来标注一个主程序类，说明是一个springboot应用
+ */
+@SpringBootApplication
+public class HelloWorldMainApplication {
+    public static void main(String[] args) {
+        //Spring应用启动起来
+        SpringApplication.run(HelloWorldMainApplication.class,args);
+    }
+}
